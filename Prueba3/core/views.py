06e6c49cfec2026d9c32da37a62821fa43028468
Rodrigo Delgado <ro.delgad>
2021-06-19 22:45:41 +0200

    Mantenedor

--- Prueba3/core/views.py
+++ Prueba3/core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Libro
 
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+def libro_lista(request):
+    libros = Libro.objects.all()
+    datos = {
+        'libros' : libros
+     }
+    return render(request, 'core/libro_lista.html', datos)
+
+def libro_form(request):
+     return render(request, 'core/form_libro')   
