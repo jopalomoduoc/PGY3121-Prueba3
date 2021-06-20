31529778d7d5bac108a0a8b5956f6fcab72861c5
Jose Palomo <josepalriv@gmail.com>
2021-06-20 00:27:55 -0400

    finalizar CRUD

--- Prueba3/core/urls.py
+++ Prueba3/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home, libro_lista, form_libro
+from .views import home, libro_lista, form_libro, form_mod_libro, form_del_libro
 
 urlpatterns = [
     path('', home, name="home"),
     path('libro-lista', libro_lista, name="libro_lista"),
     path('form-libro', form_libro, name="form_libro"),
+    path('form-mod-libro/<id>', form_mod_libro, name="form_mod_libro"),
+    path('form-del-libro/<id>', form_del_libro, name="form_del_libro"),
 ]
--- Prueba3/core/views.py
+++ Prueba3/core/views.py
@@ -1,25 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Libro
 from .forms import LibroForm
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'core/home.html')
+
+
 def libro_lista(request):
     libros = Libro.objects.all()
     datos = {
-        'libros' : libros
+        'libros': libros
      }
     return render(request, 'core/libro_lista.html', datos)
+
 
 def form_libro(request):
     datos = {
 
-        'form':LibroForm()
+        'form': LibroForm()
     }
     if request.method == 'POST':
         formulario = LibroForm(request.POST)
         if formulario.is_valid:
             formulario.save()
             datos['mensaje'] = "Datos Ingresados correctamente"
-    return render(request, 'core/form_libro.html', datos)   
+    return render(request, 'core/form_libro.html', datos)
+
+
+def form_mod_libro(request, id):
+    libro = Libro.objects.get(ISBN=id)
+    datos = {
+        'form': LibroForm(instance=libro)
+    }
+    if request.method == 'POST':
+        formulario = LibroForm(data=request.POST, instance=libro)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Modificado correctamente"
+    return render(request, 'core/form_mod_libro.html', datos)
+
+def form_del_libro(request, id):
+    libro = Libro.objects.get(ISBN=id)
+    libro.delete()
+    return redirect(to="libro_lista")
